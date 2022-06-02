fcd8055f44c33be7754c11df297c53f159f1053f
ggavy_MAC <sunutf@gmail.com>
2022-06-03 00:51:30 +0900

    [Done] solve unkown issue(expand image size)

--- client/components/imageEditor/CanvasPreview.tsx
+++ client/components/imageEditor/CanvasPreview.tsx
@@ -15,27 +15,29 @@ export async function canvasPreview(
     throw new Error('No 2d context');
   }
 
-  const scaleX = image.naturalWidth / image.width;
-  const scaleY = image.naturalHeight / image.height;
+  const scaleX = image.naturalWidth / image.width ;
+  const scaleY = image.naturalHeight / image.height ;
+  console.log(scale);
+
   // devicePixelRatio slightly increases sharpness on retina devices
   // at the expense of slightly slower render times and needing to
   // size the image back down if you want to download/upload and be
   // true to the images natural size.
-  const pixelRatio = window.devicePixelRatio;
-  // const pixelRatio = 1
+  // const pixelRatio = window.devicePixelRatio;
+  const pixelRatio = 1
 
   canvas.width = Math.floor(crop.width * scaleX * pixelRatio);
   canvas.height = Math.floor(crop.height * scaleY * pixelRatio);
-
+  
   ctx.scale(pixelRatio, pixelRatio);
   ctx.imageSmoothingQuality = 'high';
 
   const cropX = crop.x * scaleX;
   const cropY = crop.y * scaleY;
-
+  
   const rotateRads = rotate * TO_RADIANS;
-  const centerX = image.naturalWidth / 2;
-  const centerY = image.naturalHeight / 2;
+  const centerX = image.naturalWidth / 2 ;
+  const centerY = image.naturalHeight / 2 ;
 
   ctx.save();
 
@@ -62,8 +64,6 @@ export async function canvasPreview(
   );
 
   ctx.restore();
-  return new Promise(resolve => {
-    resolve(canvas.toDataURL('image/jpeg'));
-  });
-  // return canvas;
+
+  return canvas;
 }
