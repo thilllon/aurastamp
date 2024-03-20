69c71199421e58966d06915cca393e87ad471d1a
Jongmin Lee <jakelee@woowahan.com>
2024-03-20 09:29:21 +0900

    feat: remove aurastamp web lagacy

--- web-legacy/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import { GetServerSideProps } from 'next';
-
-export default function IndexPage() {
-  return null;
-}
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return { props: {}, redirect: { destination: '/decode' } };
-};
